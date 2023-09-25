fc51d46e58a847af64b37c2f1c814772517b2686
ViniciusQueirozdeJesus <133425967+ViniciusQueirozdeJesus@users.noreply.github.com>
2023-09-25 20:53:02 +0000

    adiciona `README.md`

--- website/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Member
-
-class MemberForm(forms.ModelForm): 
-    class Meta:
-        model = Member
-        fields = ['name', 'email', 'number', 'occupation', 'passwd']
