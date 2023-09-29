b67305fbb17f4601ffb856de3b876f2ea217363e
ViniciusQueirozdeJesus <133425967+ViniciusQueirozdeJesus@users.noreply.github.com>
2023-09-29 20:05:11 +0000

    atualiza `djangodb`

--- members/views.py
+++ members/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import SignUpForm
 
 #Método de cadastro
 def signup(request):
+    form = SignUpForm()
+
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = SignUpForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data['username']
@@ -15,13 +18,16 @@ def signup(request):
             login(request, user)
             messages.success(request, ("Registrado com sucesso!"))
             return redirect('home')
+        else:
+            messages.success(request, ("Houve um erro no seu registro"))
+            return render(request, 'authenticate/signup.html', {
+                'form':form,
+            })
     else:
-        messages.success(request, ("Houve um erro no seu registro"))
-        form = UserCreationForm()
 
-    return render(request, 'authenticate/signup.html', {
-        'form':form,
-    })
+        return render(request, 'authenticate/signup.html', {
+            'form':form,
+        })
 
 #Método de login
 def signin(request):
@@ -29,10 +35,10 @@ def signin(request):
     if request.method == "POST":
         #Obtém os dados do formulário
         user_login = request.POST['user_login']
-        passwd = request.POST['passwd']
+        password = request.POST['password']
 
         #Sistema do django de autenticação com os dados obtidos
-        user = authenticate(request, username=user_login, password=passwd)
+        user = authenticate(request, username=user_login, password=password)
         if user is not None:
             #Login com a autenticação do django e redireciona para a homepage
             login(request, user)
