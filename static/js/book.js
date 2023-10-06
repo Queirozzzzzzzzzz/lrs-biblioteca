e797dbfa38a8d118ad842f9888e41bb0319da7d9
aluno <vinicius.queiroz.sesisenaisp@gmail.com>
2023-10-06 15:53:42 -0300

    finaliza `sprint1`

--- static/js/book.js
+++ static/js/book.js
@@ -1,22 +1,38 @@
 var modal = document.getElementById("id_book-modal");
 var btn = document.getElementById("id_book_modal_button");
 var span = document.getElementsByClassName("close")[0];
-var book_title = document.getElementById("book-modal-title");
-var book_author = document.getElementById("book-modal-author");
-var book_synopsis = document.getElementById("book-modal-synopsis");
-var book_release_date = document.getElementById("book-modal-release_date");
-var book_is_available = document.getElementById("book-modal-is_available");
+var book_form = document.getElementById("id_book_modal_form")
 
 // Modal de Livro
 
 // Abrir modal
 function OpenBookModal(title, author, synopsis, release_date, is_available) {
   modal.style.display = "block";
-  book_title.innerHTML = title;
-  book_author.innerHTML = author;
-  book_synopsis.innerHTML = synopsis;
-  book_release_date.innerHTML = release_date;
-  book_is_available.innerHTML = is_available;
+  book_form.innerHTML = `
+    <div class="form-group">
+      <form>
+        <h1>Informações</h1>
+        <ul>
+          <li>
+            <input type="text" name="title" maxlength="255" required="" id="id_title" value="${title}" readonly>
+          </li>
+          <li>
+            <input type="text" name="author" maxlength="80" required="" id="id_author" value="${author}" readonly>
+          </li>
+          <li>
+            <textarea name="synopsis" cols="40" rows="10" required="" id="id_synopsis" readonly>${synopsis}</textarea>
+          </li>
+          <li>
+            <label for="id_release_date"><strong>Data de Lançamento:</strong> ${release_date}</label>
+          </li>
+          <li>
+            <label for="id_is_available"><strong>Disponível</strong></label>
+            <input type="checkbox" name="is_available" id="id_is_available" ${is_available ? 'checked' : ''} disabled>
+          </li>
+        </ul>
+      </form>
+    </div>
+  `;
 }
 
 // Fechar modal
