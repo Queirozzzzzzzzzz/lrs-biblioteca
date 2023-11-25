57de3b2433c28275b098d903ec82b55667b6b3bb
Vinícius Queiroz <vinicius.queiroz.sesisenaisp@gmail.com>
2023-11-25 09:28:19 -0300

    atualiza `build_files.sh`

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,4 @@
-apt-get install libcairo2-dev
+npm install libcairo2-dev
 pkg-config --atleast-version=1.12.2 cairo
 pip install -r requirements.txt
 python manage.py collectstatic --no-input --clear
